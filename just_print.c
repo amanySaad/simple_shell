c9d5cb279e5197284a527f1411a17bfd2c0a3447
amanySaad <eng.am.aboelella@gmail.com>
2023-09-09 10:30:14 -0700

    Write a beautiful code that passes the Betty checks

---
+++ just_print.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+/**
+* Write a beautiful code that passes the Betty checks
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+	printf("This will pass betty inshaallah,\n");
+	return (0);
+}
